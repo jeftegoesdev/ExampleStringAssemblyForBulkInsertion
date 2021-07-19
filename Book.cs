8d655e7c2200e3631c974bd459778e198b981986
jeftegoesdev <jefte.goes.dev@outlook.com>
2021-07-18 22:48:04 -0300

    First commit.

---
+++ Book.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExampleStringAssemblyForBulkInsertion
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
